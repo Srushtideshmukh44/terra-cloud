aea0fc478ebad36c2f16f35209f875c7fb211f2a
Srushti Deshmukh <srushtideshmukh.scoe.it@gmail.com>
2025-05-17 12:35:41 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,16 +1,2 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-variable "ami_id" {
-  description = "AMI ID to use for the EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
+variable "access_key" {}
+variable "secret_key" {}
